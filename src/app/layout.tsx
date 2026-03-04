aa65b0e9a0612b228a819d809491fe66e031594e
Alexander <sasha41187@gmail.com>
2026-03-04 22:36:42 +0300

    change font directory and font connection.0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,49 @@
 import type { Metadata } from "next";
 import { Header } from "@/components/header";
-import "@/styles/fonts.scss";
 import "@/styles/index.scss";
 import "@/styles/_reset.scss"
+
+import localFont from 'next/font/local'
+
+const openSans = localFont({
+  src: [
+    { path: './fonts/openSans/OpenSans-Regular.woff2', weight: '400', style: 'normal' },
+    { path: './fonts/openSans/OpenSans-Italic.woff2',  weight: '400', style: 'italic' },
+    { path: './fonts/openSans/OpenSans-Light.woff2',   weight: '300', style: 'normal' },
+    { path: './fonts/openSans/OpenSans-Medium.woff2',  weight: '500', style: 'normal' },
+    { path: './fonts/openSans/OpenSans-SemiBold.woff2',weight: '600', style: 'normal' },
+    { path: './fonts/openSans/OpenSans-Bold.woff2',    weight: '700', style: 'normal' },
+    { path: './fonts/openSans/OpenSans-ExtraBold.woff2',weight: '800', style: 'normal' },
+  ],
+  variable: '--font-open-sans',
+})
+
+const openSansCondensed = localFont({
+  src: [
+    { path: './fonts/openSans/OpenSansCondensed-Light.woff2',   weight: '300', style: 'normal' },
+    { path: './fonts/openSans/OpenSansCondensed-Regular.woff2', weight: '400', style: 'normal' },
+    { path: './fonts/openSans/OpenSansCondensed-Medium.woff2',  weight: '500', style: 'normal' },
+    { path: './fonts/openSans/OpenSansCondensed-SemiBold.woff2',weight: '600', style: 'normal' },
+    { path: './fonts/openSans/OpenSansCondensed-Bold.woff2',    weight: '700', style: 'normal' },
+    { path: './fonts/openSans/OpenSansCondensed-ExtraBold.woff2',weight: '800', style: 'normal' },
+  ],
+  variable: '--font-open-sans-condensed',
+})
+
+const openSansSemiCondensed = localFont({
+  src: [
+    { path: './fonts/openSans/OpenSansSemiCondensed-Light.woff2',   weight: '300', style: 'normal' },
+    { path: './fonts/openSans/OpenSansSemiCondensed-Regular.woff2', weight: '400', style: 'normal' },
+    { path: './fonts/openSans/OpenSansSemiCondensed-Medium.woff2',  weight: '500', style: 'normal' },
+    { path: './fonts/openSans/OpenSansSemiCondensed-SemiBold.woff2',weight: '600', style: 'normal' },
+    { path: './fonts/openSans/OpenSansSemiCondensed-Bold.woff2',    weight: '700', style: 'normal' },
+    { path: './fonts/openSans/OpenSansSemiCondensed-ExtraBold.woff2',weight: '800', style: 'normal' },
+  ],
+  variable: '--font-open-sans-semi-condensed',
+})
+
+
+
 
 
 export default function RootLayout({
@@ -11,7 +52,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${openSans.variable} ${openSansCondensed.variable}`}>
       <body>
       <Header></Header>
         {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
 import Image from "next/image";
-import styles from "./page.module.scss"
+import styles from "./page.module.scss";
 import { ProductCard } from "@/components/ProductCard";
 import { Hero } from "@/components/IndexPageSections/Hero";
 import { BeersMenu } from "@/components/IndexPageSections/BeersMenu";
-import {FoodMenu} from "@/components/IndexPageSections/FoodMenu";
+import { FoodMenu } from "@/components/IndexPageSections/FoodMenu";
 export default function Home() {
   return (
-     <div className="main-page">
-      <Hero/>
-        <section className={styles["galery"]}></section>
-        <BeersMenu></BeersMenu>
-        <FoodMenu/>
-        <section className={styles[""]}></section>
-        {/* <ProductCard id={1} name="Пиво" price={1200} country="De" imgUrl="/images/123.jpg" measurementUnit="Мл" volume={300}></ProductCard> */}
-     </div>
+    <div className={`${styles["main-page"]}`}>
+      <Hero />
+      <section className={styles["galery"]}></section>
+      <BeersMenu></BeersMenu>
+      <FoodMenu />
+      <section className={styles[""]}></section>
+      {/* <ProductCard id={1} name="Пиво" price={1200} country="De" imgUrl="/images/123.jpg" measurementUnit="Мл" volume={300}></ProductCard> */}
+    </div>
   );
 }
